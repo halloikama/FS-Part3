a7bd4798d523d018a71007c23f2191d63447be1a
Halloikama <halloikama_rick@hotmail.com>
2019-10-15 13:01:51 +0300

    updated build

---
+++ build/precache-manifest.b107c7603f10878ccd088bca748f3db2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5a96459763a431a9c648d09d457d23a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9072c9d8e83942dce21",
+    "url": "/static/css/main.95f870c0.chunk.css"
+  },
+  {
+    "revision": "7b9edd506a3228787ddd",
+    "url": "/static/js/2.a9c40383.chunk.js"
+  },
+  {
+    "revision": "a9072c9d8e83942dce21",
+    "url": "/static/js/main.ea0b81ac.chunk.js"
+  },
+  {
+    "revision": "0a3909685390b3afae4a",
+    "url": "/static/js/runtime~main.ce00a3d8.js"
+  }
+]);
